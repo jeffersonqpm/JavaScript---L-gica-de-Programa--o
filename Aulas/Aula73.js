964f1ee75484b64409dd2697d73db57149fb3a76
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-17 19:02:38 -0300

    primeiro

---
+++ Aulas/Aula73.js
@@ -0,0 +1,25 @@
+// declarando variaveis
+
+// 3 tipos
+
+function Aula(){
+
+   let nome = confirm('Clieque nesta  buceta')
+
+   if(nome == true){
+
+    alert("Clicou em OK")
+
+   }else{
+    alert("Clicou em Cancelar")
+   }
+
+ 
+
+
+
+}
+
+
+
+

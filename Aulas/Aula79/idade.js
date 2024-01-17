6a5981903d76d4e9cd4dfd9311dd1ed026c4f0da
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-17 20:47:42 -0300

    update

---
+++ Aulas/Aula79/idade.js
@@ -0,0 +1,12 @@
+let nomeVelha = prompt("Digigite seu nome velho(a)")
+let idadeVelha = prompt("Digite sua idade velho(a)")
+
+let nomeNova = prompt('Digite seu nome novo(a)')
+let idadeNova =  prompt('Digite sua idade novo(a)')
+
+let diferencaIdade = idadeVelha - idadeNova
+
+alert("Nome da pessoa mais velha: " + nomeVelha)
+alert("Nome da pessoa mais nova: " + nomeNova)
+alert("A diferença de idade é de " + diferencaIdade + " anos.")
+

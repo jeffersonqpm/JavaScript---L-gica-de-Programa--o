6529f6fd9216f359e815ca8312ae345db7c91226
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-17 23:21:44 -0300

    alteração

--- Aulas/Aula79 Ex03/idade.js
+++ Aulas/Aula79 Ex03/idade.js
@@ -1,12 +1,32 @@
-let nomeVelha = prompt("Digigite seu nome velho(a)")
-let idadeVelha = prompt("Digite sua idade velho(a)")
+let nomeVelha = prompt("Digite o nome da pessoa mais velha")
+if(nomeVelha == null){
 
-let nomeNova = prompt('Digite seu nome novo(a)')
-let idadeNova =  prompt('Digite sua idade novo(a)')
+    alert('Você cancelou!')
+    
+}else{
+    let idadeVelha = prompt("Digite a idade da pessoa mais velha")
 
-let diferencaIdade = idadeVelha - idadeNova
+    let nomeNova = prompt('Digite a idade da pessoa mais velha')
+    let idadeNova =  prompt('Digite a idade da pessoa mais nova')
+    
+    let diferencaIdade = idadeVelha - idadeNova
+    
+    alert(
+        "Nome da pessoa mais velha:\n " + nomeVelha +
+        "\n\n Nome da pessoa mais nova: \n" + nomeNova+
+        "\n\n A diferença de idade é de \n" + diferencaIdade + 
+        " anos."
+        )
 
-alert("Nome da pessoa mais velha: " + nomeVelha)
-alert("Nome da pessoa mais nova: " + nomeNova)
-alert("A diferença de idade é de " + diferencaIdade + " anos.")
+
+
+
+
+    // alert("Nome da pessoa mais nova: " + nomeNova)
+    // alert("A diferença de idade é de " + diferencaIdade + " anos.")
+    
+    // alert()
+}
+
+
 
